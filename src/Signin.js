5101072215a8539326183871985fa104d4db9fc0
raki0125 <113659973+raki0125@users.noreply.github.com>
2023-01-23 18:28:44 +0530

    creating a user data base and setting docs

--- src/Signin.js
+++ src/Signin.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { signInWithGooglePopup } from "./firebase";
+import { signInWithGooglePopup, createUserDocumentFromAuth } from "./firebase";
 
 const Signin = () => {
   const logGoogleUser = async () => {
-    const response = await signInWithGooglePopup();
-    console.log(response)
+    const {user} = await signInWithGooglePopup();
+ const userDocRef= await createUserDocumentFromAuth(user)
+    console.log(logGoogleUser);
   };
   return (
     <div>
--- src/firebase.js
+++ src/firebase.js
@@ -6,6 +6,9 @@ import {
   signInWithPopup,
   signInWithRedirect,
 } from "firebase/auth";
+
+import { getFirestore, doc, getDoc, setDoc, Firestore } from "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDqK00WLE-YFfF0HrFIIQ_rSRzmjjV6Lro",
   authDomain: "e-commerce-b44e6.firebaseapp.com",
@@ -17,9 +20,71 @@ const firebaseConfig = {
 
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
+const provider = new GoogleAuthProvider();  
 provider.setCustomParameters({
-  prompt: "select_account",
-});
+  prompt:"select_account"
+})
+
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const signInWithGooglePopup = ()=> signInWithPopup(auth,provider);
+
+
+// initilizing firestore
+
+export const db = getFirestore();                              
+export const createUserDocumentFromAuth = async(userAuth)=> { 
+  const userDocRef = doc(db,'users',userAuth.uid)             
+  const userSnapShot = await getDoc(userDocRef)      
+  console.log(userSnapShot)       
+
+// user data does not exist so creating user data with setDoc that means creating a data in firestore
+
+// if(!userSnapShot.exists()){
+//  const {displayName , email }= userAuth
+//  const createdAt = new Date();
+
+// try {
+//   await setDoc(userDocRef,{
+//     displayName,
+//     email,
+//     createdAt
+//   })
+  
+// } catch (error) {
+//   console.log('error creating a user', error.message);
+// }
+
+
+// }
+
+// return userDocRef; // why we want to return if exist we are going to return
+
+
+if(!userSnapShot.exists()){
+  const{email, displayName} = userAuth;
+  const createdAt = new Date()
+
+try{
+await setDoc(userDocRef, {
+email,
+      displayName,
+      createdAt
+    });
+
+  }catch(err)
+  {
+console.log('errror while creating a user',err.message);
+  }
+}
+} 
+
+
+
+
+
+
+// creating database
+//creating variable that is getting a request from db
+//creating doc to getting a details note : doc containes user details for eg: user API 
+// getDoc is getting a actual detailes from firestore
